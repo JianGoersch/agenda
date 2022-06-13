581d9b212d36cc732b2cdfe258771afbc2c2f550
JianGoersch <jian.goersch@gmail.com>
2022-06-13 17:51:59 -0300

    Criado tabela de compromissos para trabalhar com a aegnda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core.models import Compromisso
 
-# Register your models here.
+
+class CompromissoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ('titulo','usuario', 'data_evento', )
+
+# Register your models
+
+admin.site.register(Compromisso, CompromissoAdmin)
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Compromisso(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento: ')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) #associando usuário ao evento(Chave estrangeira) ondelete cascade é para excluir todos os eventos caso o user seja deletado
+
+    class Meta:
+        db_table = 'compromisso'
+
+    def __str__(self):
+        return self.titulo
